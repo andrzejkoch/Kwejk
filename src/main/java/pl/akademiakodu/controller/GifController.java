1d2d88f9cf10c3000cfe82197fc2b80b34ee5e51
andrzejkoch <andrzejkoch@poczta.onet.pl>
2018-09-22 13:01:57 +0200

    first commit

---
+++ src/main/java/pl/akademiakodu/controller/GifController.java
@@ -0,0 +1,14 @@
+package pl.akademiakodu.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+@Controller
+public class GifController {
+    @RequestMapping("/")
+    public String listGifs() {
+        return "home";
+    }
+}
